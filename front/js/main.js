fbe32f697452ef63b037cd068a2a48ddd4693e60
olivier <jo.menoube@gmail.com>
2021-06-01 20:09:39 +0200

    fetch dices from server

--- front/js/main.js
+++ front/js/main.js
@@ -16,8 +16,25 @@ var app = new Vue({
 })
 
 async function getThrowResults(throws) {
+
+
   if (!Number.isInteger(throws)) throw 'Paramater is not an integer';
-  let response = fetch('/api/getDiceResults', {faces: 6, count: throws});
-  let data = await response.json();
-  return data;
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({"faces":10,"count":6});
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+
+  var response = await fetch("http://localhost:5000/api/getDiceResults", requestOptions);
+  const result = await response.json();
+  return result;
+
+
 }
