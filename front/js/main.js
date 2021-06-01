0c3d0971c956bfb2a303cb718b35907979d4170e
olivier <jo.menoube@gmail.com>
2021-06-01 17:50:45 +0200

    show dice result

--- front/js/main.js
+++ front/js/main.js
@@ -1,17 +1,30 @@
 var app = new Vue({
   el: '#dice',
   data: {
-    result: 0
+    result: "Pas de dé lancé",
   },
   methods: {
-    throw_dices : async function getThrowResults() {
-      const result = await getDiceResult(6);
-      app.result = result;
+    throw_dices : async function () {
+      throws = await getThrowResults(2);
+      result = ""
+      for (var i = 0; i < throws.length; i++) {
+        result = result + `${throws[i]} `;
+      }
+      this.result = result;
     }
   }
 })
 
+function getThrowResults(throws) {
+  if (!Number.isInteger(throws)) throw 'Paramater is not an integer';
+  console.log(throws);
+  results = [];
+  for (var i = 0; i < throws; i++) {
+    results.push(getDiceResult(6));
+  }
 
+  return results;
+}
 function getDiceResult(max) {
   return Math.floor(Math.random() * max)
 }
