892ea63f0e2cc5458eba334b4debe66d687970f4
olivier <jo.menoube@gmail.com>
2021-06-05 20:02:08 +0200

    call sesssion new & join

--- front/js/home.js
+++ front/js/home.js
@@ -3,9 +3,34 @@ function initApp() {
   home = new Vue({
     el: "#app",
     data: {
+      sessionName : "Pas de données",
+      playerName : "Pas de nom"
     },
     methods: {
-      play_game :  function() {
+      play_game :  async function(game) {
+        var response = await fetch("/session/new", {
+          method: 'POST',
+          redirect: 'follow'
+        });
+        const result = await response.json();
+        this.sessionName = result.name;
+        console.log(result);
+
+        // create a standard player for creator of session game
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({"name": "Plop",
+                                  "avatar": "azcsde",
+                                  "sessionName": result.name});
+        console.log(raw);
+        var response_player = await fetch("/session/join", {
+          method: 'POST',
+          redirect: 'follow',
+          headers: myHeaders,
+          body: raw,
+        });
+        const player = await response_player.json();
+        console.log(player);
       }
     }
   });
