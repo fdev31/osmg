3cbccd92bc900e47c21aba90697ec7bf9e0b84ee
fdev31 <fdev31@gmail.com>
2021-06-05 21:17:08 +0200

    update the room access

--- back/back/staticfiles.py
+++ back/back/staticfiles.py
@@ -2,8 +2,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 
 async def roomsAccess(sessionId):
-    return RedirectResponse('/static/rooms/test.html?foo')
+    return RedirectResponse('/static/join.html?session='+sessionId)
 
 def init(app, config):
     app.mount("/static", StaticFiles(directory=config.static_dir), name="static")
-    app.mount("/r/{sessionId}", roomsAccess)
+    app.get("/r/{sessionId}")(roomsAccess)
