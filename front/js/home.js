edb001fd2603748c2e1dcebaae10a59547d0c416
olivier <jo.menoube@gmail.com>
2021-06-05 19:24:14 +0200

    fix initapp

--- front/js/home.js
+++ front/js/home.js
@@ -1,10 +1,11 @@
 
 function initApp() {
   home = new Vue({
+    el: "#app",
+    data: {
+    },
     methods: {
-      play_game :  function(event) {
-        console.log("test");
-        console.log(e);
+      play_game :  function() {
       }
     }
   });
