d3c1d25cad834a354763be7572b328d9609a48a2
olivier <jo.menoube@gmail.com>
2021-06-06 11:24:34 +0200

    update dice to new frame

--- front/js/dice.js
+++ front/js/dice.js
@@ -1,15 +1,12 @@
 
 function initApp() {
   marathon = new Vue({
-    Vue.component('marathon', {
-      data: function() {
-        return {
+      el: "#app",
+      data: {
           player_action : "Lancer le dé",
           result: "Pas de dé lancé",
           remain : 42195,
           choice : 0,
-        }
-
       },
       methods: {
         throw_dices : async function () {
@@ -27,6 +24,39 @@ function initApp() {
           this.player_action =`Lancer le dé`;
         }
       }
-    })
-  })
+  });
+}
+
+function showResults(throws) {
+  result = ""
+  for (var i = 0; i < throws.length; i++) {
+    result = result + `${throws[i]} `;
+  }
+  return result;
+}
+async function getThrowResults(throws) {
+
+
+  if (!Number.isInteger(throws)) throw 'Paramater is not an integer';
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({"faces":6,"count":throws});
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+
+  var response = await fetch("http://localhost:5000/api/getDiceResults", requestOptions);
+  const result = await response.json();
+  return result;
+
+
+}
+async function check_choice(choice) {
+  return true
 }
