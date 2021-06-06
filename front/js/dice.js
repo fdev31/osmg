728738584a286bc6ca0f5910a28880d65ed8b095
olivier <jo.menoube@gmail.com>
2021-06-06 12:40:20 +0200

    retract kilometer , check user choice

--- front/js/dice.js
+++ front/js/dice.js
@@ -1,28 +1,47 @@
-
+// Il faut déterminer comment se lance le jeu . Cela nécessite de répérer le créateur de la session. Vérifier la présence de chacun au début et pendant le jeu
+// Il faut vérifier que chacun soit synchrone avec l'état de la partie
+// Quand dois je utiliser l'api diceAction
+// Il me faut les données pour ne plus hardcoder , de quoi rafraichir l'état du jeu relancer session join
 function initApp() {
+  try {
+    var cookie = JSON.parse(document.cookie.split('; ')[0]);
+  } catch (e) {
+    window.location = "http://localhost:5000/static/index.html";
+  }
+
+  if (typeof cookie.name == "undefined" && cookie.name == null) {
+    window.location = "http://localhost:5000/static/saloon.html";
+  }
   marathon = new Vue({
       el: "#app",
       data: {
           player_action : "Lancer le dé",
           result: "Pas de dé lancé",
+          dice_throws : [],
           remain : 42195,
           choice : 0,
+          game : cookie
       },
       methods: {
         throw_dices : async function () {
-          throws = await getThrowResults(4);
+          this.dice_throws = await getThrowResults(4);
 
-          this.result = showResults(throws);
-          console.log(this.player_action);
+          this.result = showResults(this.dice_throws);
           this.player_action =`Avancez de ${this.player_action} km`;
-          console.log(this.player_action);
         },
         player_advance : async function() {
-          if (! await check_choice(choice)) throw "Choississez les bons dés";
-          this.remain -= this.choice;
-          console.log(this.player_action);
-          this.player_action =`Lancer le dé`;
-        }
+          console.log(arrayEquals( processStringToArrayNumber(this.choice), this.dice_throws));
+          if ( arrayEquals( processStringToArrayNumber(this.choice), this.dice_throws)) {
+            this.remain -= this.choice; //
+            console.log(this.player_action);
+            this.player_action =`Lancer le dé`;
+          }
+
+
+
+
+        },
+
       }
   });
 }
@@ -64,6 +83,23 @@ async function getThrowResults(throws) {
 
 
 }
-async function check_choice(choice) {
-  return true
+
+function processStringToArrayNumber (choice) {
+  if (typeof choice !== "string") throw "argument must be a string";
+  res = []
+  for (var i = 0; i < choice.length; i++) {
+    res.push(parseInt(choice[i]))
+  }
+  return res;
+}
+function arrayEquals(arr1 , arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) throw "arguments must be Array objects";
+  if (arr1.length != arr2.length)return false;
+  arr1.sort();
+  arr2.sort();
+  isEquals = true;
+  arr1.every(function(element, index) {
+    if (element !== arr2[index]) isEquals = false
+  })
+  return isEquals
 }
