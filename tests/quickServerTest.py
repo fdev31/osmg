19be11d59b612537b1269ce82476e2c7c319e263
fdev31 <fdev31@gmail.com>
2021-06-06 14:45:54 +0200

    update the test

--- tests/quickServerTest.py
+++ tests/quickServerTest.py
@@ -1,6 +1,20 @@
+#!/bin/env python
+
 HOST='http://localhost:5000'
 import requests
+# create a new session
 r = requests.post(HOST+'/session/new')
 data = r.json()
 
 print(data)
+
+# join a session
+
+sessionData = {
+  "name": "Toto",
+  "avatar": "monster",
+  "sessionName": data['name']
+}
+r = requests.post(HOST+'/session/join', json=sessionData)
+
+print(r.json())
