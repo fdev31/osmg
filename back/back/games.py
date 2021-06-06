a8fb95b40e4b6512c8e8a7172acd2d61d71a8846
fdev31 <fdev31@gmail.com>
2021-06-06 19:24:42 +0200

    add a listgames API

--- back/back/games.py
+++ back/back/games.py
@@ -7,12 +7,23 @@ logger = logging.getLogger()
 
 GAMES = ['marathon']
 
+gameDB = {}
+
+def listGames() -> dict:
+    " list all games by name "
+    return gameDB
+
 def init(app, config):
     for game in GAMES:
         logger.info(f"Game {game}")
         mod = importlib.import_module(f'back.gamelib.{game}')
         for gameName, api in mod.definition.items():
+            gameDB[gameName] = dict(
+                description=api.description
+            )
             registerGame(gameName, api)
             for actionName in api.actions:
                 logger.debug(f"importing {game} action: {actionName}")
                 app.post(f'/game/{gameName}/{actionName}')(api.actions[actionName])
+
+    app.get('/gamelist')(listGames)
