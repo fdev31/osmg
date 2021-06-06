a7c18538139f76eb840dab9379dfcb3eef1799b6
olivier <jo.menoube@gmail.com>
2021-06-06 10:57:51 +0200

    redirect if no cookie

--- front/js/lounge.js
+++ front/js/lounge.js
@@ -1,8 +1,16 @@
 function initApp() {
-  var value = JSON.parse(document.cookie.split('; ')[0]);
-  console.log(value);
+  try {
+    var cookie = JSON.parse(document.cookie.split('; ')[0]);
+  } catch (e) { // dirty workaround for now
+    window.location = "http://localhost:5000/static/index.html";
+  }
+
+  if (typeof cookie.name == "undefined" && cookie.name == null) {
+    window.location = "http://localhost:5000/static/index.html";
+  }
+  console.log(cookie);
   lounge = new Vue({
     el : "#app",
-    data : value
+    data : cookie
   })
 }
