6f89149e549aa47ce01698fef6d445e201de41fb
fdev31 <fdev31@gmail.com>
2021-06-08 23:55:20 +0200

    untested addition of the player id in the disconnect

--- back/back/stream.py
+++ back/back/stream.py
@@ -3,18 +3,19 @@ import logging
 from fastapi import Request
 from sse_starlette.sse import EventSourceResponse
 
-from back.sessionManager import disconnectPlayer
+from back.models import PlayerIdentifier
+from back.sessionmanager import disconnectPlayer
 from back.globalHandlers import getRedis
 
 logger = logging.getLogger("Stream")
 
-async def event_source(request, params):
+async def event_source(request, params, playerId):
     channel = getRedis().pubsub()
     await channel.subscribe(params)
     async for message in channel.listen():
         if await request.is_disconnected():
-            await disconnectPlayer()
-            logger.debug('Request disconnected')
+            await disconnectPlayer(playerId)
+            logger.debug('Stream disconnected')
             break
         if message['type'] == 'message':
             yield {
@@ -22,9 +23,9 @@ async def event_source(request, params):
                 "data": message['data'],
             }
 
-async def eventStream(topic: str, request: Request) -> EventSourceResponse:
-    " Returns an event source for the provided topic "
-    return EventSourceResponse(event_source(request, topic))
+async def eventStream(request: Request, player: PlayerIdentifier) -> EventSourceResponse:
+    " Returns an event source for the provided topic & user "
+    return EventSourceResponse(event_source(request, topic, player.id))
 
 def init(app, config):
     app.get('/stream')(eventStream)
