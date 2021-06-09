c1e136af98103a1a2b436ec214129eb9b5bb0bf3
fdev31 <fdev31@gmail.com>
2021-06-09 12:03:39 +0200

    better disconnect handling

--- back/back/stream.py
+++ back/back/stream.py
@@ -11,17 +11,20 @@ logger = logging.getLogger("Stream")
 
 async def event_source(request, params, playerId):
     channel = getRedis().pubsub()
-    await channel.subscribe(params)
-    async for message in channel.listen():
-        if await request.is_disconnected():
-            await disconnectPlayer(playerId)
-            logger.debug('Stream disconnected')
-            break
-        if message['type'] == 'message':
-            yield {
-                "event": "update",
-                "data": message['data'],
-            }
+    try:
+        await channel.subscribe(params)
+        async for message in channel.listen():
+            if await request.is_disconnected():
+                await disconnectPlayer(playerId)
+                logger.debug('Stream disconnected')
+                break
+            if message['type'] == 'message':
+                yield {
+                    "event": "update",
+                    "data": message['data'],
+                }
+    except asyncio.CancelledError as e:
+        await disconnectPlayer(playerId)
 
 async def eventStream(request: Request, player: PlayerIdentifier) -> EventSourceResponse:
     " Returns an event source for the provided topic & user "
