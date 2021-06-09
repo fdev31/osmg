743951205a6469784780c1e5357707c5c978177d
fdev31 <fdev31@gmail.com>
2021-06-09 14:39:53 +0200

    minor fix

--- back/back/sessionmanager.py
+++ back/back/sessionmanager.py
@@ -90,8 +90,8 @@ async def makeSession() -> Session:
     logger.debug(f"New session {uid}")
     return sess
 
-async def disconnectPlayer(playerId):
-    return publishEvent(player.sessionName, None, cat='disconnectPlayer', name=playerId)
+async def disconnectPlayer(playerId: int):
+    await publishEvent(player.sessionName, None, cat='disconnectPlayer', name=playerId)
     # TODO: mark as not ready
     # TODO: if no players anymore, remove the session
 
