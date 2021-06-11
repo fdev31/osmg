5bbffa98ae0ea632d79e855a8bc013e3b4eadcae
fdev31 <fdev31@gmail.com>
2021-06-11 19:48:47 +0200

    be more flexible on svg

--- source_svgs/avatarize.py
+++ source_svgs/avatarize.py
@@ -10,12 +10,20 @@ svg_fragments = []
 
 all_data = {}
 
+def extractContent(content):
+    if content.name in ('circle', 'ellipse', 'path', 'polygon'):
+        return str(content)
+    return ''
+
 def digestSVG(name, data):
     all_data['current'].append('<g class="%s">'%name)
     soup = BeautifulSoup(data, 'html.parser')
     for i, svg in enumerate(soup.find('svg')): # expect 1
-        all_data['current'].append( ''.join(str(x) for x in svg.contents))
-    assert i == 0
+        try:
+            all_data['current'].append(''.join(extractContent(x) for x in svg.contents))
+        except AttributeError:
+            pass
+#     assert i == 0
     all_data['current'].append('</g>')
 
 for root, dirs, files in os.walk('.'):
