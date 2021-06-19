002dbb186269617ab87833d3275dd98b678960d6
olivier <jo.menoube@gmail.com>
2021-06-19 22:51:00 +0200

    create dicearray component

---
+++ components/src/dicearray.vue
@@ -0,0 +1,22 @@
+<script>
+import dice from "./dice";
+
+export default {
+  components : {dice},
+  data() {
+    return{
+      dices: [],
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="dices">
+    <dice
+      v-for="index in 4"
+      :key="index"
+    />
+  </div>
+</template>
