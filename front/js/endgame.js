b80cbb41b07d0838e66f09ce2bf1f5eb1aed6d23
olivier <jo.menoube@gmail.com>
2021-06-22 17:18:18 +0200

    set a podium location

--- front/js/endgame.js
+++ front/js/endgame.js
@@ -12,21 +12,46 @@ function initApp() {
         data : function () {
             return data;
         },
-        watch: {
-            players(newVal) {
-                this.$refs.playerlist.players = newVal;
-            }
-        },
+        // watch: {
+        //     players(newVal) {
+        //         this.$refs.playerlist.players = newVal;
+        //     }
+        // },
         mounted() {
-            this.$refs.playerlist.players = this.players;
+            setPodiumLocation()
         },
         components: {"avatar-card" : window["avatar-card"] , "player-list" : window["player-list"]},
         methods : {
             findPlayer(playerId) {
               return findPlayer(this , playerId);  
-            }
+            },
+
         }
         
     });
     endgame = app.mount('#app');
+}
+
+function setPodiumLocation() {
+    let finalistsElts = document.getElementsByClassName("finalist");
+    let svg = document.getElementById("Capa_1");
+    let svgDim= svg.getBoundingClientRect();
+    console.log(svgDim);
+    for (const elt of finalistsElts) {
+        switch (0) {
+            case 0:
+                elt.style.top = svgDim.height * (1/5);elt.style.left = svgDim.width * (1/3);
+                break;
+            case 1:
+                elt.style.top = svgDim.height/2;elt.style.left = svgDim.width * (2/3);
+                break;     
+            case 2:
+                elt.style.top = svgDim.height/2;elt.style.left = 1;
+                break;                                    
+            default:
+                elt.style.top = svgDim.height/2;elt.style.left = svgDim.width * (1/3);
+                break;
+        }
+    }
+
 }
