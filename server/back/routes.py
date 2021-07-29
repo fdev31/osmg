b6d40e853839f61366a6b72650efbd052ae1f376
fdev31 <fdev31@gmail.com>
2021-07-29 19:44:20 +0200

    support a default prefix

--- server/back/routes.py
+++ server/back/routes.py
@@ -16,7 +16,7 @@ else:
     app = FastAPI(debug=False, docs_url=None, redoc_url=None, default_response_class=ORJSONResponse)
 
 config = ODict(
-    static_dir=os.path.join(os.environ['PREFIX'], 'front'),
+    static_dir=os.path.join(os.environ.get('PREFIX', os.getcwd()), 'front'),
     redis_server=os.environ.get('REDIS_SERVER', 'localhost'),
     debug=debug,
 )
