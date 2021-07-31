95489624197eed002dad127d7ff26fce755f26b9
fdev31 <fdev31@gmail.com>
2021-07-31 15:50:57 +0200

    front tests: Allow 2+ clients

--- tests_front/test_marathon.py
+++ tests_front/test_marathon.py
@@ -1,5 +1,5 @@
 import os
-import time
+from time import sleep
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,80 +7,112 @@ from selenium.webdriver.common.keys import Keys
 from config import HOST
 
 HOME_INDEX = 0
+NB_PLAYERS = 6
 
-if not os.path.exists('screenshots'):
-    os.mkdir('screenshots')
+if not os.path.exists("screenshots"):
+    os.mkdir("screenshots")
+
 
 def shot(driver, name):
-    driver.save_screenshot(os.path.join("screenshots", f'marathon_{name}.png') )
+    driver.save_screenshot(os.path.join("screenshots", f"marathon_{name}.png"))
+
+
+def makeShots(drivers, suffix):
+    shot(drivers[0], "ff_end_screen")
+    for i, drv in enumerate(drivers[1:]):
+        shot(drv, "ch%d_%s" % (i + 2, suffix))
+
 
 def setInputText(inp, value, delete=None):
     inp.click()
-    time.sleep(0.1)
+    sleep(0.1)
     if delete:
-        inp.send_keys([Keys.BACKSPACE]*delete)
+        inp.send_keys([Keys.BACKSPACE] * delete)
     else:
         inp.send_keys([Keys.CONTROL, "a"])
-    time.sleep(0.1)
+    sleep(0.1)
     inp.send_keys(value)
+
 
 def create_game(driver, gameIndex):
     driver.get("http://" + HOST)
 
     # change login
     setInputText(driver.find_element_by_tag_name("input"), "S. Anita")
+    sleep(1)
 
     # click game tile
-    driver.find_elements_by_class_name('gamebutton')[gameIndex].click()
+    but = driver.find_elements_by_class_name("gamebutton")[gameIndex]
+    but.click()
+
 
 class MarathonTest(unittest.TestCase):
-
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver2 = webdriver.Chrome()
+        self.drv = [webdriver.Firefox()]
+        sleep(1)
+        for n in range(1, NB_PLAYERS):
+            self.drv.append(webdriver.Chrome())
+            sleep(1)
+        sleep(0.5)
+
+    @property
+    def driver(self):
+        return self.drv[0]
 
     def test_game(self):
         create_game(self.driver, HOME_INDEX)
-        lobby_url = self.driver.find_element_by_id('link').get_attribute('value')
+        sleep(1)
+        lobby_url = self.driver.find_element_by_id("link").get_attribute("value")
         shot(self.driver, "login1")
         # other player join game
-        self.driver2.get(lobby_url)
-        time.sleep(0.5)
-        setInputText(self.driver2.find_element_by_tag_name("input"), "pif paf", delete=10)
-        shot(self.driver2, "login2")
-        self.driver2.find_elements_by_tag_name("button")[0].click()
-        time.sleep(1)
-        # start game
-        self.driver.find_elements_by_class_name("mainAction")[0].click()
-        self.driver2.find_elements_by_class_name("mainAction")[0].click()
+        for drv in self.drv[1:]:
+            drv.get(lobby_url)
+        sleep(0.5)
+        for i, drv in enumerate(self.drv[1:]):
+            setInputText(
+                drv.find_element_by_tag_name("input"), "Player %d" % i, delete=10
+            )
+            shot(drv, "login%d" % (i + 2))
+            drv.find_elements_by_tag_name("button")[0].click()
 
-        time.sleep(8)
-        shot(self.driver, "ff_game")
-        shot(self.driver2, "ch_game")
-        but_idx = 1
+        print("About to start")
+        sleep(1)
+
+        # start game
+        for drv in self.drv:
+            drv.find_elements_by_class_name("mainAction")[0].click()
+
+        sleep(8)
+        print("Start")
+
+        def playerTurn(drv):
+            drv.find_elements_by_class_name("mainAction")[0].click()
+            sleep(0.5)
+            drv.find_elements_by_class_name("mainAction")[0].click()
+            sleep(0.5)
+
         for n in range(20):
             try:
-                self.driver.find_elements_by_class_name("mainAction")[0].click()
-                time.sleep(0.1)
-                self.driver.find_elements_by_class_name("mainAction")[0].click()
-                time.sleep(0.5)
-                self.driver2.find_elements_by_class_name("mainAction")[0].click()
-                time.sleep(0.1)
-                self.driver2.find_elements_by_class_name("mainAction")[0].click()
-                time.sleep(0.5)
-            except IndexError:
-                shot(self.driver, "ff_end_game")
-                shot(self.driver2, "ch_end_game")
+                for drv in self.drv:
+                    playerTurn(drv)
+                sleep(0.5)
+            except IndexError as e:
+                print("game end detected", e)
+                makeShots(self.drv, "end_game")
                 break
             else:
                 if n == 10:
-                    shot(self.driver, "ff_mid_game")
-                    shot(self.driver2, "ch_mid_game")
-        shot(self.driver, "ff_end_screen")
-        shot(self.driver2, "ch_end_screen")
+                    makeShots(self.drv, "mid_game")
+            sleep(0.1 * NB_PLAYERS)
+
+        makeShots(self.drv, "end_screen")
 
     def tearDown(self):
-        self.driver.close()
+        if "WAIT" in os.environ:
+            input("Press some key to continue...")
+        for drv in self.drv:
+            drv.close()
+
 
 if __name__ == "__main__":
     unittest.main()
