bff041199cc2089707e2621346dc3e156fd5cd49
fdev31 <fdev31@gmail.com>
2021-08-01 19:10:52 +0200

    add simple fade to playerlist & dice array usage

--- components/src/player-list.vue
+++ components/src/player-list.vue
@@ -3,40 +3,56 @@ import avatarCard from "./avatar-card";
 
 export default {
   components: { avatarCard },
-  emits : ['kick'],
+  emits: ["kick"],
   data() {
     return {
       players: [],
-
     };
   },
-  props : {
-    enableKick : Boolean,
-    curPlayer : String,
-    myId : Number
+  props: {
+    enableKick: Boolean,
+    curPlayer: String,
+    myId: Number,
   },
-  methods : {
-    isPlaying : function(player) {
-      if (this.curPlayer != undefined && parseInt(player.id) === parseInt(this.curPlayer)) return true
+  methods: {
+    isPlaying: function (player) {
+      if (
+        this.curPlayer != undefined &&
+        parseInt(player.id) === parseInt(this.curPlayer)
+      )
+        return true;
       else return false;
     },
-  }
+  },
 };
 </script>
 
 <style scoped lang="less"></style>
 
 <template>
-  
-  <div v-for="item in players" :key="item.id" :class="{players : true , playing : isPlaying(item) ,disconnected : item.disconnected}">
-    <avatar-card
-      
-      class="avatar-lobby"
-      :avatar-name="item.name"
-      :avatar-id="item.id"
-    />
-    <div> 
-      <button v-on:click="$emit('kick', item , 'true')" v-if="enableKick && parseInt(item.id) != parseInt(myId)">Kick player</button>
+  <transition-group name="fade">
+    <div
+      v-for="item in players"
+      :key="item.id"
+      :class="{
+        players: true,
+        playing: isPlaying(item),
+        disconnected: item.disconnected,
+      }"
+    >
+      <avatar-card
+        class="avatar-lobby"
+        :avatar-name="item.name"
+        :avatar-id="item.id"
+      />
+      <div>
+        <button
+          v-on:click="$emit('kick', item, 'true')"
+          v-if="enableKick && parseInt(item.id) != parseInt(myId)"
+        >
+          Kick player
+        </button>
+      </div>
     </div>
-  </div>
+  </transition-group>
 </template>
