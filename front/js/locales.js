06c223ac49195f069f6047846ddc4870dc806431
fdev31 <fdev31@gmail.com>
2022-06-02 19:20:36 +0200

    update locales

--- front/js/locales.js
+++ front/js/locales.js
@@ -1,10 +1,13 @@
 const locales = {
-  fr: {
+  en: {
+    marathon_rules:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     marathon: "Marathon",
-    marathon_description: "Un jeu de course avec des d\u00e9s",
+    marathon_description: "A racing game with dices!",
     marathon_long_description:
-      "Affrontez vos amis pour des grands moments de d\u00e9lire!<br/>Un m\u00e9lange fun de hasard et de maths de base.",
-    "Marathon , the dice game": "Marathon , le jeu de d\u00e9",
+      "Race against your opponents for a big moment of fun <br/>A mix of chance and simple maths.",
+  },
+  fr: {
     marathon_rules:
       "Le but du jeu est de parcourir EXACTEMENT 42195 m\u00e8tres. Vous perdez quand vous parcourez plus 42195m\u00e8tres. Lancez les d\u00e9s et choisissez combien de m\u00e8tre vous d\u00e9cidez d'avancer.",
     "Change game": "Changez de jeu",
@@ -25,12 +28,11 @@ const locales = {
     "Error ": " Erreur",
     "meters left": "m\u00e8tres restants",
     meters: "m\u00e8tres",
-    Welcome: "Bienvenue",
-    "Your info": "Vos infos",
-    "Pick a game": "Choisissez un jeu",
-    "Kick player": "Expulser jouer",
-    Ready: "Pr\u00eat",
-    "Not ready": "Pas pr\u00eat",
+    marathon: "Marathon",
+    marathon_description: "Un jeu de course avec des d\u00e9s",
+    marathon_long_description:
+      "Affrontez vos amis pour des grands moments de d\u00e9lire!<br/>Un m\u00e9lange fun de hasard et de maths de base.",
+    "Marathon , the dice game": "Marathon , le jeu de d\u00e9",
     players: "joueurs",
     Invite: "Inviter",
     Play: "Jouer",
@@ -43,13 +45,11 @@ const locales = {
     "End of vote. Player stay in game": "Fin du vote. Le joueur reste en jeu",
     "Click to copy invite link to the clipboard":
       "Cliquez pour copier le lien d'invitation dans le presse papier",
-  },
-  en: {
-    marathon: "Marathon",
-    marathon_description: "A racing game with dices!",
-    marathon_long_description:
-      "Race against your opponents for a big moment of fun <br/>A mix of chance and simple maths.",
-    marathon_rules:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Welcome: "Bienvenue",
+    "Your info": "Vos infos",
+    "Pick a game": "Choisissez un jeu",
+    "Kick player": "Expulser jouer",
+    Ready: "Pr\u00eat",
+    "Not ready": "Pas pr\u00eat",
   },
 };
