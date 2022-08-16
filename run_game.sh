1968804b155723cbd6a74af2fc4b48214c72e648
fdev31 <fdev31@gmail.com>
2022-08-16 23:56:39 +0200

    pass extra parameters used in websockets

--- run_game.sh
+++ run_game.sh
@@ -3,15 +3,16 @@ PY=python3
 ENVDIR=.tox/py310-chrome
 
 HTTP_PORT=$(cat HTTP_PORT)
+PARAMS="--ws-ping-interval 5 --ws-ping-timeout 2"
 
 source ${ENVDIR}/bin/activate
 export PREFIX=$(pwd)
 if [ -z "$DEBUG" ]; then
     export WEB_CONCURRENCY=10
     echo "Running standard mode"
-    exec $VIRTUAL_ENV/bin/uvicorn back.routes:app --port $HTTP_PORT --log-level=warning --log-config logging.yaml
+    exec $VIRTUAL_ENV/bin/uvicorn back.routes:app $PARAMS --port $HTTP_PORT --log-level=warning --log-config logging.yaml
 else
     echo "Running debug mode"
     export DEBUG
-    exec $VIRTUAL_ENV/bin/uvicorn back.routes:app --host 0.0.0.0 --reload --port $HTTP_PORT --log-level=debug --log-config logging.yaml
+    exec $VIRTUAL_ENV/bin/uvicorn back.routes:app $PARAMS --host 0.0.0.0 --reload --port $HTTP_PORT --log-level=debug --log-config logging.yaml
 fi
