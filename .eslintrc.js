30611ca1040e114a890d06fae0190706ae16f180
fdev31 <fdev31@gmail.com>
2022-09-03 13:47:17 +0200

    ignore the linting for the vite config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
-  ignorePatterns: [".eslintrc.js", "postcss.config.js", "tailwind.config.js"],
+  ignorePatterns: [
+    ".eslintrc.js",
+    "postcss.config.js",
+    "tailwind.config.js",
+    "vite.config.js",
+  ],
   rules: {
     // 'vue/no-unused-vars': 'error'
   },
